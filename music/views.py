3b0fb3e73c15d2c1a507cc07262c970cb54af69e
rajat-rjt <rajat.gupta2017@vitstudent.ac.in>
2018-12-01 23:26:19 +0530

    album view

--- music/views.py
+++ music/views.py
@@ -9,6 +9,12 @@ from django.shortcuts import render
 # This is the easy way to do things manually to understand the concept ::::
 
 
+def album_detail(request, artist_id):
+	artist = Artist.objects.get(pk=artist_id)
+	all_albums = Album.objects.all()
+	context = {
+		'all_albums': all_albums,
+		'artist': artist
 
-
-
+	}
+	return render(request, 'music/album.html', context)
